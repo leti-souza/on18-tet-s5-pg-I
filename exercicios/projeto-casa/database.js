bc3997969c5b2791020caebf621c72b5979463bb
leti-souza <leticia.souzaprogramacao@gmail.com>
2022-09-19 12:59:29 -0300

    Resolucao projeto guiado

---
+++ exercicios/projeto-casa/database.js
@@ -0,0 +1,31 @@
+const livros = [{
+    titulo: "Livro de Filosofia da Computacao",
+    categoria: "tecnologia",
+    paginas: 200,
+    leu: true,
+    recomenda: true
+},
+{
+    titulo: "Biografia Conceição Evaristo",
+    categoria: "biografia",
+    paginas: 360,
+    leu: true,
+    recomenda: true
+},
+{
+    titulo: "Teto para Dois",
+    categoria: "Romance",
+    paginas: 400,
+    leu: true,
+    recomenda: true
+},
+
+{
+    titulo: "A sutil arte de ligar o foda-se",
+    categoria: "autoajuda",
+    paginas: 224,
+    leu: false,
+    recomenda: false
+}
+]
+module.exports = livros
